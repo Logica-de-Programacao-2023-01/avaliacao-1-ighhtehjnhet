41b319cfcbee42b3472997b51cf15e5a4c3b4f00
Eduardo Dias Moreira Aragão <eduardo.aragao@dinfor.uniceub>
2023-04-24 11:07:05 -0300

    resolvido

---
+++ q3/questão n3.go
@@ -0,0 +1,30 @@
+package q3
+
+import "errors"
+
+func main() {
+	var peça int
+	peça = 2
+
+	var resultado int
+	tamanhoTab = n + n
+if n <=0{
+return 0,errors.New("error)
+}else if n=0{
+   return 0,errors.New("error")
+
+}
+ tamanhoTab = n + n
+
+resultado = tamanhoTab / peça
+
+if tamanhoTab%2 == 0{
+
+return resultado, nil
+
+}else {
+}
+tamanhoTab -= 1
+
+return resultado, nil{
+}
---
+++ q5/questao 5.go
@@ -0,0 +1,15 @@
+package q5
+
+func ProcessString(s string) string
+// Seu código aqui
+// Deletar todas as vogais da string vowels="AEIOUaeiou"
+for_v=range vowels{
+	str=string.replayAll(str,string(v))
+	consonants:= "BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz"for_,c :=rangeconsonants{
+		str=strings, replaceAll(str,string(c), "."+string(c)){
+			for i=0;i<len(str)i++{
+				if "A"<=str[i] && str[i]<='Z' {str=str[:i]+string (str[i]+32)+str[i+1]}
+}
+}
+}
+}
